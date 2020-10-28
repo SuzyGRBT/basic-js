7a31ee5201fc70bb6286b49e3a11590c389c2ace
SuzyGRBT <lady94@inbox.ru>
2020-10-28 23:07:15 +0300

    feat: imlement task count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  let arr = [];
+  matrix.forEach(elem => arr = arr.concat(elem));
+
+  let catsCount = 0;
+  arr.forEach(el => el === "^^" ? catsCount++ : catsCount);
+
+  return catsCount;
 };
